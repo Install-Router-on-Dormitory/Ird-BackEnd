065589ac59f9654ff1755aad955c1162fbd096ca
Huuuunee <s21024@gsm.hs.kr>
2022-12-21 10:05:24 +0900

    :tada: init

---
+++ src/test/kotlin/com/ird/demo/DemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ird.demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
