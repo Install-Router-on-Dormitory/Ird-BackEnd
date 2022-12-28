2ed59824dc4006ee432e178c748686fccd73bbc6
Huuuunee <s21024@gsm.hs.kr>
2022-12-28 15:05:19 +0900

    :sparkles: SecurityConfig 추가

---
+++ src/main/kotlin/com/ird/demo/global/security/config/security/SecurityConfig.kt
@@ -0,0 +1,58 @@
+package com.ird.demo.global.security.config.security
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.ird.demo.global.config.filter.FilterConfig
+import com.ird.demo.global.security.CustomAuthenticationEntryPoint
+import com.ird.demo.global.security.jwt.JwtTokenProvider
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.util.matcher.RequestMatcher
+import org.springframework.web.cors.CorsUtils
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig(
+    private val jwtTokenProvider: JwtTokenProvider,
+    private val objectMapper: ObjectMapper,
+) {
+    @Bean
+    fun filterChain(http: HttpSecurity): SecurityFilterChain {
+        return http
+            .cors().and()
+            .csrf().disable()
+            .formLogin().disable()
+            .httpBasic().disable()
+            .sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+            .and()
+            .authorizeRequests()
+            .requestMatchers(RequestMatcher { request ->
+                CorsUtils.isPreFlightRequest(request)
+            }).permitAll()
+
+            .antMatchers(HttpMethod.GET, "/auth/login").permitAll()
+            .antMatchers(HttpMethod.GET, "/auth/redirect").permitAll()
+
+            .anyRequest().denyAll()
+            .and()
+            .exceptionHandling()
+            .authenticationEntryPoint(CustomAuthenticationEntryPoint(objectMapper))
+
+            .and()
+            .apply(FilterConfig(jwtTokenProvider, objectMapper))
+            .and()
+            .build()
+    }
+
+    @Bean
+    fun passwordEncoder(): PasswordEncoder =
+        BCryptPasswordEncoder()
+}
