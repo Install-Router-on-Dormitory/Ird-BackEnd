ec068d653c73dca9e93ad6c617a8e148ea403ea7
Huuuunee <s21024@gsm.hs.kr>
2022-12-28 15:02:08 +0900

    :sparkles: exception 처리 + 필터 추가

---
+++ src/main/kotlin/com/ird/demo/global/error/handler/GlobalExceptionHandler.kt
@@ -0,0 +1,25 @@
+package com.ird.demo.global.error.handler
+
+import com.example.helloworld.global.error.exception.BasicException
+import com.ird.demo.global.error.ErrorResponse
+import org.slf4j.LoggerFactory
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+import javax.servlet.http.HttpServletRequest
+
+@RestControllerAdvice
+class GlobalExceptionHandler(
+) {
+
+    private val log = LoggerFactory.getLogger(this.javaClass.simpleName)
+
+    @ExceptionHandler(BasicException::class)
+    fun basicExceptionHandler(request: HttpServletRequest, ex: BasicException): ResponseEntity<ErrorResponse> {
+        log?.error(request.requestURI)
+        log?.error(ex.message)
+        val errorResponse = ErrorResponse(ex.errorCode)
+        return ResponseEntity<ErrorResponse>(errorResponse, HttpStatus.valueOf(ex.errorCode.code))
+    }
+}
